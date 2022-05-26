3b6a2fd7ce347f01a7beffa75afd8b81e10d9a59
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-05-27 00:42:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import instaloader
 
-bot1 = instaloader.Instaloader()
+try:
+   bot1 = instaloader.Instaloader()
 
-username = st.text_input("Enter Profile Username")
+   username = st.text_input("Enter Profile Username")
 
-st.write(bot1.download_profile(username,profile_pic_only=True))
+   st.write(bot1.download_profile(username,profile_pic_only=True))
+except:
+  pass
