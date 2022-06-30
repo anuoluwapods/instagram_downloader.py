0de05985053191f34619ec01a0e6c948f3229905
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-30 22:01:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import instaloader
+from PIL import image
+
+image = Image.open('image.png')
+
+col1, col2 = st.solumns(2)
+
+col1.header("Instragram Profile Picture Downloader")
+col2.image(image)
 
 try:
    bot1 = instaloader.Instaloader()
 
    username = st.text_input("Enter Profile Username")
 
-   st.write(bot1.download_profile(username,profile_pic_only=True))
+   st.write(bot1.download_profile(username, profile_pic_only=True))
 except:
   pass
